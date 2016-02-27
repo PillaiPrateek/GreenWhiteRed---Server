d5ecf74c5a566ea983deccf5ca4b720607de54cd
Matt-Doyle <doy0001@jmss.vic.edu.au>
2016-02-27 16:09:32 +1100

    Made CSS work with .ejs files.

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express = require("express");
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use("/styles", express.static(__dirname + "/styles"));
 
 var controller = require("./controller")(app);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+});
+
+app.get('/login', function(request, response) {
+	response.render('pages/login.ejs');
 });
